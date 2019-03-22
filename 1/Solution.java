788851c2895e31e62322516c6ea137b773f0ee15
resoff <res-off@mail.ru>
2019-03-22 14:57:27 +0700

    Update Solution.java

--- 1/Solution.java
+++ 1/Solution.java
@@ -1,24 +1,31 @@
-﻿import java.util.*;
+﻿import java.util.Scanner;
 
 public class Solution {
 	/*
-	*  Дано n - число уровней. Построить треугольник из символов #.
-	*  Пример для n = 4:
-	*	   #	
-	*	  ##
-	*	 ###
-	*	####
-	*/	 
-	    
+	 *  Дано n - число уровней. Построить треугольник из символов #.
+	 *  Пример для n = 4:
+	 *	   #
+	 *	  ##
+	 *	 ###
+	 *	####
+	 */
+
 	static void func1(int n) {
-     
-       }
-    
-	private static final Scanner scanner = new Scanner(System.in);
+
+		int c = 0;
+		for (int i = 0; i < n; i++) {
+			for(int t = 0; t < n; t++)
+			{
+				System.out.print(c < t ? "#" : " ");
+			}
+			System.out.println();
+		}
+
+	}
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        func1(n);
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        func1(4);
 	}	
