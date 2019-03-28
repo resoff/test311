c0f789cc21bba2f516123bf233b6db2ecf336cde
resoff <res-off@mail.ru>
2019-03-28 13:56:59 +0700

    Update Solution.java

--- 9/Solution.java
+++ 9/Solution.java
@@ -1,51 +1,55 @@
-import java.io.*;
-import java.util.*;
 
-public class Solution {
 
 	/*
-	* Реализовать функцию вычисляющую высоту дерева. Дерево растет в два цикла каждый год. Каждую весну его высота удваевается,
-	* каждое лето дерево прибавляет один метр. Дерево высаживается высотой 1 метр. 
-	* Вычислить высоту дерева через n - циклов роста.
-	*
-	* Дано: t - количество тестовых примеров
-	*	n - количество циклов роста
-	*  	1 <= t <= 10
-	* 	0 <= t <= 60
-	* Пример:
-	*       2
-	*	0
-	*	4
-	*
-	* Решение:
-	*	1
-	*	7
-	*/
-	
-    static int func(int n) {
-   
-    }
+	 * Реализовать функцию вычисляющую высоту дерева. Дерево растет в два цикла каждый год. Каждую весну его высота удваевается,
+	 * каждое лето дерево прибавляет один метр. Дерево высаживается высотой 1 метр.
+	 * Вычислить высоту дерева через n - циклов роста.
+	 *
+	 * Дано: t - количество тестовых примеров
+	 *	n - количество циклов роста
+	 *  	1 <= t <= 10
+	 * 	0 <= t <= 60
+	 * Пример:
+	 *       2
+	 *	0
+	 *	4
+	 *
+	 * Решение:
+	 *	1
+	 *	7
+	 */
 
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+	import java.util.Scanner;
 
-        int t = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	public class Solution {
 
-        for (int tItr = 0; tItr < t; tItr++) {
-            int n = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		static void func1(int n, int cycles[])
+		{
+			int c;
+			for(int k = 0; k < n; k++)
+			{
+				c = cycles[k];
+				int lenWood = 1; // высота дерева
+				for (int i = 0; i < c; i++)
+				{
+					lenWood = i % 2 == 0 ? lenWood * 2 : lenWood + 1;
+				}
+				System.out.println(lenWood);
+			}
+		}
 
-            int result = func(n);
+		public static void main(String[] args)
+		{
+			Scanner scanner = new Scanner(System.in);
+			int n = scanner.nextInt();
 
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
-        }
+			int[] cycles = new int[n]; //создаем массив тестовых примеров
 
-        bufferedWriter.close();
-
-        scanner.close();
-    }
-}
+			for (int i = 0; i < n; i++) //считываем в массив
+			{
+				cycles[i] = scanner.nextInt();
+			}
+			func1(n, cycles);
+		}
+	}
